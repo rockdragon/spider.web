789409346407217ee410a522a6bfb4f63c788138
Locke <moyerock@gmail.com>
2014-12-31 21:07:07 +0800

    add watcher to get config dynamically.:

--- modules/config/configUtils.js
+++ modules/config/configUtils.js
@@ -3,11 +3,12 @@ var path = require('path');
 
 var cfgFileName = 'config.cfg';
 var cache = {};
+var filePath = path.join(process.env.spider_web, cfgFileName);
 
 function getConfigs() {
     if (!cache[cfgFileName]) {
         if (!process.env.cloudDriveConfig) {
-            process.env.cloudDriveConfig = path.join(process.env.spider_web, cfgFileName);
+            process.env.cloudDriveConfig = filePath;
         }
         if (fs.existsSync(process.env.cloudDriveConfig)) {
             var contents = fs.readFileSync(process.env.cloudDriveConfig, {encoding: 'utf-8'});
@@ -18,6 +19,19 @@ function getConfigs() {
 }
 module.exports.getConfigs = getConfigs;
 
-module.exports.isDevelopment = function(){
-    return getConfigs().node_env  === 'development';
+function fileChanged(curr, prev) {
+    if (curr.mtime !== prev.mtime) {
+        console.log('config has been changed. curr mtime is: ',
+            curr.mtime, 'prev mtime was: ' + prev.mtime);
+        if (fs.existsSync(filePath)) {
+            var contents = fs.readFileSync(filePath, {encoding: 'utf-8'});
+            cache[cfgFileName] = JSON.parse(contents);
+            console.log(cache[cfgFileName]);
+        }
+    }
+}
+fs.watchFile(filePath, fileChanged);
+
+module.exports.isDevelopment = function () {
+    return getConfigs().node_env === 'development';
 };
